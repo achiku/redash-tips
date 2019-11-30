cfe5c7a2b8705d4447d5dfa616f7b0649855bc2f
Akira Chiku <akira.chiku@gmail.com>
2019-11-30 19:18:56 +0900

    Update

--- create-data.sql
+++ create-data.sql
@@ -59,3 +59,65 @@ from (
 ) d  
 order by d.tm
 ;
+
+
+insert into adjust_install_event (installed_at, network_name)
+select
+  tm
+  , ('{A,A,A,A,B,B,C,Organic,Organic,Organic}'::text[])[ceil(random()*10)]
+from (
+  -- very small
+  select
+    tm
+  from dummy_time
+  tablesample bernoulli(6.5) -- 5*1.3
+  where (
+    extract(hour from tm at time zone 'jst') >= 3 and extract(hour from tm at time zone 'jst') < 6
+  )
+  union
+  -- small
+  select
+    tm
+  from dummy_time
+  tablesample bernoulli(26) -- 20*1.3 = 26
+  where (
+    extract(hour from tm at time zone 'jst') >= 2 and extract(hour from tm at time zone 'jst') < 3
+  ) or (
+    extract(hour from tm at time zone 'jst') >= 6 and extract(hour from tm at time zone 'jst') < 8
+  )
+  union
+  -- normal
+  select
+    tm
+  from dummy_time
+  tablesample bernoulli(32.5) -- 25*1.3 = 32.5
+  where (
+    extract(hour from tm at time zone 'jst') >= 0 and extract(hour from tm at time zone 'jst') < 2
+  ) or (
+    extract(hour from tm at time zone 'jst') >= 8 and extract(hour from tm at time zone 'jst') < 12
+  ) or (
+    extract(hour from tm at time zone 'jst') >= 14 and extract(hour from tm at time zone 'jst') < 22
+  )
+  union
+  -- large
+  select
+    tm
+  from dummy_time
+  tablesample bernoulli(52) -- 40*1.3 = 52
+  where (
+    extract(hour from tm at time zone 'jst') >= 12 and extract(hour from tm at time zone 'jst') < 14
+  ) or (
+    extract(hour from tm at time zone 'jst') >= 22 and extract(hour from tm at time zone 'jst') <= 24
+  )
+) d  
+order by d.tm
+;
+
+
+insert into adjust_registration_event (registered_at, network_name, user_id)
+select
+  registered_at
+  , ('{A,A,A,A,B,B,C,Organic,Organic,Organic}'::text[])[ceil(random()*10)]
+  , id
+from your_service_user
+;
